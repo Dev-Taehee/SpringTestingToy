14a197d1d58a39b58286603e2396bbbd8652c628
TaeheeWoo94 <wth0086@gmail.com>
2023-10-16 15:31:35 +0900

    first commit

---
+++ testingToy/src/main/java/com/testingToy/TestingToyApplication.java
@@ -0,0 +1,13 @@
+package com.testingToy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingToyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingToyApplication.class, args);
+	}
+
+}
---
+++ testingToy/src/test/java/com/testingToy/TestingToyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testingToy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingToyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
